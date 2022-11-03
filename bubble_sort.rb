254c573218c8add71fde885fcc9f4b912b6c8b01
ekaterina <katyabalash@mail.ru>
2022-11-03 17:33:07 +0300

    Delete first while loop

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,7 @@
-$array = [6,29,65,0,4,8,2,22,45,100,1]
+$array = [4,3,78,2,0,2]
 def bubble_sort
     new_array = []
     final_array = []
-    length = $array.length
-    i = 0
-    while i < length do
 i=0
 while i<$array.length do
    if $array[1] == nil
@@ -18,7 +15,6 @@ while i<$array.length do
     new_array.push($array[0])
     $array.delete_at(0)
    end
-end
 end
 p "Sorted array #{final_array}"
 end
